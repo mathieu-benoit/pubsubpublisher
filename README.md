4fc9246fbd8256ec4f4b05cb9affe1ad55bf0b40
Mathieu Benoit <mabenoit@google.com>
2021-06-21 16:14:12 -0400

    read NUMBER_OF_MESSAGES and DELAY_BETWEEN_MESSAGES from env vars

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 .NET Core app to publish messages on PubSub, leveraging [this sample code](https://cloud.google.com/pubsub/docs/publisher#c).
+
+```
+docker build -t pubsubpublisher .
+docker run -it -e DELAY_BETWEEN_MESSAGES="1000" -e NUMBER_OF_MESSAGES="5" pubsubpublisher
+```
+
+Other resources:
+- [.NET Core samples with PubSub](https://github.com/GoogleCloudPlatform/dotnet-docs-samples/tree/master/pubsub/api/Pubsub.Samples)
+- [PubSub load test framework](https://github.com/GoogleCloudPlatform/pubsub/tree/master/load-test-framework/)
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,13 @@
 using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello World!");
+var counter = 0;
+var max = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("NUMBER_OF_MESSAGES")) ? Int32.Parse(Environment.GetEnvironmentVariable("NUMBER_OF_MESSAGES")) : 0;
+var delay = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DELAY_BETWEEN_MESSAGES")) ? Int32.Parse(Environment.GetEnvironmentVariable("DELAY_BETWEEN_MESSAGES")) : 0;
+while (counter < max)
+{
+    Console.WriteLine("Hello, World!");
+    counter++;
+    await Task.Delay(delay);
+}
